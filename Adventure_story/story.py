a4f31f6e921987467bcf7c4b5757d52cbe832cf1
gboladee <gboladeadeniji@gmail.com>
2022-11-17 23:31:56 +0100

    Adventure!

---
+++ Adventure_story/story.py
@@ -0,0 +1,11 @@
+# Congratulations, you are going to create your own adventure story!
+# This program makes you them main character, and will customize the story to suit your interest!
+print("Welcome to your adventure simulator!")
+print("I am going to ask you a bunch of questions and then create an epic story with you as the star!")
+
+name = input("What is your name?   \n")
+enemy = input ("What is your worst enemy’s name?   \n")
+superpower = input ("What is your superpower?   \n")
+address = input ("Where do you live?  \n")
+food = input ("What is your favorite food?   \n")  
+print("Hello ", name,  " Your ability to ", superpower , "will make sure you never have to look at" , enemy , "again. Go eat", food , " as you walk down the streets of " , address , " and use", superpower , "for good and not evil!")
